c58fc67553245fefda24a38b3a512a36d2163362
PoulaHelmy <bolahelmy2016@gmail.com>
2022-04-11 01:23:45 +0200

    finish Generators lab

--- apps/login/src/app/remote-entry/entry.module.ts
+++ apps/login/src/app/remote-entry/entry.module.ts
@@ -2,16 +2,13 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RemoteEntryComponent} from './entry.component';
 import {RouterModule} from '@angular/router';
-import {FeatLoginModule} from "@bg-hoard/feat-login/feat-login.module";
-import {MatCardModule} from "@angular/material/card";
-import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
+
 
 @NgModule({
   declarations: [RemoteEntryComponent],
   imports: [
     CommonModule,
-    MatCardModule,
-    StoreUiSharedModule,
+
     RouterModule.forChild([
       {
         path: '',
