e4d5c554e4d59fdde8cb0bf79303172f250a9233
PoulaHelmy <bolahelmy2016@gmail.com>
2022-04-11 00:48:25 +0200

    CcC

--- apps/dashboard/mfe.config.js
+++ apps/dashboard/mfe.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   name: 'dashboard',
-  remotes: {
-    todo: 'https://todo-mfe.netlify.app/remoteEntry.mjs',
-    login: 'https://login-mfe.netlify.app/remoteEntry.mjs',
-  },
-
+  remotes: [['todo', 'https://todo-mfe.netlify.app/remoteEntry.mjs'], ['login', 'https://login-mfe.netlify.app/remoteEntry.mjs']]
 };
