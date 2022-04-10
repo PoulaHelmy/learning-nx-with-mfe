4a2d8283c3aa3155989f5ab64bd3f9c45f775b5e
PoulaHelmy <bolahelmy2016@gmail.com>
2022-04-11 00:32:50 +0200

    chagnens

--- apps/dashboard/mfe.config.js
+++ apps/dashboard/mfe.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   name: 'dashboard',
-  remotes: {
-    todo: 'https://todo-mfe.netlify.app/remoteEntry.js',
-    login: 'https://login-mfe.netlify.app/remoteEntry.js',
-  },
-  shared: ({
-    "@bg-hoard/shared/auth": {
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: "auto",
-      eager: true //true means it will be in my remoteEntry.js
-    }
-  }),
+  remotes: ['todo', 'login']
 };
--- apps/login/mfe.config.js
+++ apps/login/mfe.config.js
@@ -2,13 +2,5 @@ module.exports = {
   name: 'login',
   exposes: {
     './Module': 'apps/login/src/app/remote-entry/entry.module.ts',
-  },
-  shared: ({
-    "@bg-hoard/shared/auth": {
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: "auto",
-      eager: true //true means it will be in my remoteEntry.js
-    }
-  }),
+  }
 };
